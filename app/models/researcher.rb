de690cf8d146341731f282f77873607f5f027f5f
David Barsky <barskydavid@gmail.com>
2015-02-12 19:43:45 -0500

    created Researcher model

---
+++ app/models/researcher.rb
@@ -0,0 +1,6 @@
+class Researcher < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
