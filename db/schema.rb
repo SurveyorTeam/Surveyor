cde409899e995f26c640ffb2df5fec2347c5386e
David Barsky <barskydavid@gmail.com>
2015-03-02 19:46:44 -0500

    removed duplicate button

--- db/schema.rb
+++ db/schema.rb
@@ -66,34 +66,7 @@ ActiveRecord::Schema.define(version: 20150301180431) do
   add_index "research_subjects", ["email"], name: "index_research_subjects_on_email", unique: true, using: :btree
   add_index "research_subjects", ["reset_password_token"], name: "index_research_subjects_on_reset_password_token", unique: true, using: :btree
 
-  create_table "researchees", force: :cascade do |t|
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "gender"
-    t.string   "state"
-    t.integer  "age"
-    t.integer  "income"
-    t.integer  "education_level"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "researchees", ["email"], name: "index_researchees_on_email", unique: true, using: :btree
-  add_index "researchees", ["reset_password_token"], name: "index_researchees_on_reset_password_token", unique: true, using: :btree
-
   create_table "researchers", force: :cascade do |t|
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -104,7 +77,6 @@ ActiveRecord::Schema.define(version: 20150301180431) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "organization"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
