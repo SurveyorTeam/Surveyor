3789c9cbdbdc788eeada031ce80f98c133da7b8f
David Barsky <barskydavid@gmail.com>
2015-03-02 22:00:22 -0500

    created project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303014903) do
+ActiveRecord::Schema.define(version: 20150303022901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150303014903) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "studies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/studies.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :study do
+    title "A sample study"
+    description "Used to find out what students think about sherman"
+  end
+end
