47f58ef106c731dcdbd8c72570ce87d6289fb972
David Barsky <barskydavid@gmail.com>
2015-03-02 20:58:42 -0500

    updated home page new user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'fabrication'
-Researchee.destroy_all
-Researcher.destroy_all
-100.times do Fabricate(:researchee) end
-15.times do Fabricate(:researcher) end
-# 100.times do 
- 	#researchee = Researchee.create(first_name: Faker::Name.first_name,email:"email@email.com",password:"password",gender:"unicorn",age:"12",education_level:"3",state:"MA")
-# end
