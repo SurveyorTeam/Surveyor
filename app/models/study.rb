fb18dd394491644091c20a9ce12ac2d0859b0d15
David Barsky <barskydavid@gmail.com>
2015-03-03 12:11:13 -0500

    passing studies model tests

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,3 +1,4 @@
 class Study < ActiveRecord::Base
   belongs_to :researcher
+  validates :title, presence: true
 end
--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Study, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory" do
+    FactoryGirl.build(:study).should be_valid
+  end
+
+  it "should require a title" do
+    FactoryGirl.build(:study, :title => "").should_not be_valid
+  end
 end
