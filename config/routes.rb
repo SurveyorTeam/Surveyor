567430efeafbb94489267ed5dbc93ccaed01cd27
KK <ursinewalrus@yahoo.com>
2015-03-16 00:49:50 -0400

    added subjects table, added demographic atributes to surveys

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :subjects
+
   resources :survey_responses
 
   resources :responses
--- db/migrate/20150303210742_create_surveys.rb
+++ db/migrate/20150303210742_create_surveys.rb
@@ -2,6 +2,14 @@ class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
       t.string :name
+      #demographic info
+      t.integer :gender
+      t.integer :age
+      t.integer :education_level
+      t.integer :income
+      t.string :race
+      t.string :nationality
+      t.string :state
 
       t.timestamps null: false
       t.belongs_to :projects
