06e851db746125fc4918a71207b4045ec30fc911
KK <ursinewalrus@yahoo.com>
2015-03-21 17:54:00 -0400

    can now add text responses to questions, no link off the page it leads you to afterwards though, if anyone is feeling adventurous they could try to add other response type capability

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,5 @@
 class Question < ActiveRecord::Base
 
-  def self.add_responses(responses,response_type)
-    puts responses.class
-    responses.each do |k,v|
-      if response_type == 'text'
-        Question.new(:text => v, :question_id => k)
-      end #add bool, multi, etc
-    end
-
-  end
   belongs_to :survey
   has_many :survey_responses
 end
