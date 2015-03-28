7febbeadf2a64d1f9c6d1edefb93aa3aebe66a15
Tiffany Chen <tiffany.wenya.chen@gmail.com>
2015-03-28 08:20:42 -0400

    Version that we showed Pito yesterday. Survey creation remains broken. View for survy creation slightly modified from last commit.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -27,22 +27,20 @@ class SurveysController < ApplicationController
     
   end
   
-  def create_survey
-    puts "********212121****"
-    puts params
-  end
-  
   def survey_respond
     @current_survey = Survey.find(params[:id])
     @current_questions = Question.where(:survey_id => @current_survey.id)
   end
+  
   def submit_responses
     text_responses = params['text']
     Survey_response.add_responses(text_responses,'text')
     #text_responses = params['bool'] you get the idea, one for each new implemented type of question
     #QUestion.add_responses(bool_responses, 'bool')
   end
+  
   # POST /surveys
+  
   def create
     #@current_study = #{current_study} -- need to pass current study in, possibly when linking from creating a study
     #doesn't properly do studies either yet
@@ -52,7 +50,18 @@ class SurveysController < ApplicationController
     if @survey.save
       redirect_to @survey, notice: 'survey was successfully created.'
     else
+     render :new
+   end
+ end
+  
+  def create_survey
+    @survey = Survey.new(survey_params) 
+    
+    if @survey.save
+      redirect_to @survey, notice: 'survey was successfully created.'
+    else
       render :new
+      puts "fail"
     end
   end
 
@@ -79,7 +88,10 @@ class SurveysController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def survey_params
-      params.require(:survey).permit(:title, :description)
+      puts "********88*********"
+      puts params.inspect
+     puts "********88*********"
+     params.require(:description).permit(:text)
     end
 
     def user_only
