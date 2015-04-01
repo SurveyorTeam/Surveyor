18d9d6214e36d677580bfd5538fc010fd5f574d9
David Barsky <barskydavid@gmail.com>
2015-04-01 15:34:54 -0400

    started working with demogrpahics

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,29 @@
 Rails.application.routes.draw do
   
+  # survey specifics
   resources :demographics
+  resources :responses
+  resources :questions
+  resources :projects 
+  resources :surveys
+  resources :user_responses
+  resources :studies
+  resources :survey_responses
+  
+  # users
+  resources :subjects
 
   devise_for :subjects, controllers: {
     sessions:'subjects/sessions'
   }
-  
-  resources :subjects
-  
-  resources :survey_responses
 
   get '/submit_survey_responses' => 'surveys#submit_responses', as: :post
   
   get '/create_survey' => 'surveys#create_survey'
 
-  resources :responses
-
-  resources :questions
-
-  resources :projects 
-
-  resources :surveys 
-
   get 'surveys/:id/survey_respond' => 'surveys#survey_respond'
   
   get 'projects/:id/survey/new' => 'surveys#new'
-  
-  resources :user_responses
-
-  resources :studies
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
