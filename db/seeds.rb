f6b2114dd8b7d7392b84a4506d3112957ad029f0
Yoseph Tesfagaber <yosepht@brandeis.edu>
2015-05-06 11:00:20 -0400

    seed data for a proper survey

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,44 @@ SurveyResponse.create(text: 'burgers',question_id: 1)
 SurveyResponse.create(text: 'Leftovers',question_id: 1)
 SurveyResponse.create(text: 'Floor change',question_id: 1)
 SurveyResponse.create(text: 'Duck',question_id: 1)
+
+
 33.times do SurveyResponse.create(bool: 'A',question_id: 3) end
 45.times do SurveyResponse.create(bool: 'B',question_id: 3) end
 50.times do SurveyResponse.create(bool: 'A',question_id: 7) end
 1.times do SurveyResponse.create(bool: 'B',question_id: 7) end
+
+
+Project.create(title: 'Brandeis Political Opinions', description:'A preliminary examination of undergraduate students opinions on the 2016 elections', user_id: 1)
+
+Survey.create(name:'Democractic Leaning of Undergraduate Males', gender: 'Male', education_level:'Any', nationality:'USA', projects_id: 2)
+
+#questions 8 - 14
+Question.create(text:'Are you of voting age?', kind: 1, boolean_option_1: 'Yes', boolean_option_2:'No', survey_id: 3)
+Question.create(text:'Have you previously voted for a democractic candidate?', kind: 1, boolean_option_1:'Yes', boolean_option_2:'No', survey_id: 3)
+Question.create(text:'If the vote was today, which candidate would you vote for?', kind: 0, survey_id: 3)
+Question.create(text:'What is the most important issue to you?', kind: 0, survey_id: 3)
+Question.create(text:'Do you feel fully informed about the potential candidates?', kind: 1, boolean_option_1:'Yes', boolean_option_2:'No', survey_id: 3)
+Question.create(text:'Would you be willing to donate to a candidate who only aligned on the previous issue?', kind:1, boolean_option_1: 'Yes', boolean_option_2: 'No',survey_id:3)
+#8
+13.times do SurveyResponse.create(bool: 'A', question_id: 8) end
+7.times do SurveyResponse.create(bool: 'B', question_id: 8) end
+#9
+12.times do SurveyResponse.create(bool: 'A', question_id: 9) end
+8.times do SurveyResponse.create(bool: 'B', question_id: 9) end
+#10
+8.times do SurveyResponse.create(text: 'Sanders', question_id: 10) end
+7.times do SurveyResponse.create(text: 'Hillary', question_id: 10) end
+5.times do SurveyResponse.create(text: 'Warren', question_id: 10) end
+#11
+4.times do SurveyResponse.create(text: 'Economy', question_id: 11) end
+5.times do SurveyResponse.create(text: 'Education', question_id: 11) end
+6.times do SurveyResponse.create(text: 'Immigration', question_id: 11) end
+2.times do SurveyResponse.create(text: 'The children', question_id: 11) end
+3.times do SurveyResponse.create(text: 'Abortion', question_id: 11) end
+#12
+6.times do SurveyResponse.create(bool: 'A', question_id: 12) end
+14.times do SurveyResponse.create(bool: 'B', question_id: 12) end
+#13
+10.times do SurveyResponse.create(bool: 'A', question_id: 13) end
+10.times do SurveyResponse.create(bool: 'B', question_id: 13) end
